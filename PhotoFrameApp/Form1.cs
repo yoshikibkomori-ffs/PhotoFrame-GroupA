956ca2ea9e0a4bc9085e87672d75e2806931d382
tomoyakimura-ffs <tomoya.kimura@fujifilm.com>
2018-07-13 12:28:48 +0900

    form1を変更

--- PhotoFrameApp/Form1.cs
+++ PhotoFrameApp/Form1.cs
@@ -23,6 +23,7 @@ namespace PhotoFrameApp
         private PhotoRepository photoRepository;
         private PhotoFileService photoFileService;
         private List<Photo> photos;
+        bool order = false;
 
         public Form1(KeywordRepository keywordRepository, PhotoRepository photoRepository, PhotoFileService photoFileService)
         {
@@ -42,7 +43,7 @@ namespace PhotoFrameApp
             }
         }
 
-        private void Display_Click(object sender, EventArgs e)
+        private void Click_Display(object sender, EventArgs e)
         {
             ListView1.Items.Clear();
             this.photos = Application.createPhotoList();
@@ -94,7 +95,7 @@ namespace PhotoFrameApp
                         PhotoDateTime = "";
                     }
 
-                    string[] item = { Path.GetFileName(photo.filePath), KeyWord, isFavorite, PhotoDateTime };
+                    string[] item = { Path.GetFileName(photo.File.FilePath), KeyWord, isFavorite, PhotoDateTime };
                     ListView1.Items.Add(new ListViewItem(item));
                 }
                 
@@ -103,241 +104,103 @@ namespace PhotoFrameApp
             
         }
 
-        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void Update_PhotoList(int index, Photo photo)
         {
 
         }
 
-        private void DateTimePicker2_ValueChanged(object sender, EventArgs e)
+        private void Click_StartDateTimePicker(object sender, EventArgs e)
         {
 
         }
 
-        private void SelectDate_Click(object sender, EventArgs e)
+        private void Click_EndDateTimePicker(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_SelectDate(object sender, EventArgs e)
         {
 
             DateTime s_Date = DateTimePicker1.Value;
             DateTime e_Date = DateTimePicker2.Value;
-            this.photos = Application.SearchDate(this.photos, s_Date, e_Date);
+            this.photos = Application.searchDate(this.photos, s_Date, e_Date);
             Set_PhotoList();
         }
+
+        private void KeywordBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_SelectKeyword(object sender, EventArgs e)
+        {
+            string keytext = KeywordBox1.SelectedText;
+            this.photos = Application.searchKeyword(this.photos, keytext);
+            Set_PhotoList();
+        }
+
+        private void Click_SelectFavorite(object sender, EventArgs e)
+        {
+            this.photos = Application.searchFavorite(this.photos);
+            Set_PhotoList();
+        }
+
+        private void KeywordBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_AddKeyword(object sender, EventArgs e)
+        {
+            string keytext = KeywordBox2.Text;
+            Application.addKeyword(keytext);
+            foreach (Photo photo in this.photos)
+            { 
+            Application.changeKeyword(photo, keytext);
+            }
+            //インデックスの再表示処理
+
+        }
+
+        private void Click_AddFavorite(object sender, EventArgs e)
+        {
+            //foreach(Photo photo in this.photos)
+            //{
+            //    Application.changeFavorite(photo);
+            //}
+            //インデックスの再表示処理
+
+            for(int i = 0; i < ListView1.SelectedItems.Count; i++)
+            {
+                int index = ListView1.SelectedItems[i].Index;
+                Photo photo = this.photos.ElementAt(index);
+            }
+        }
+
+        private void Click_SlideShow(object sender, EventArgs e)
+        {
+            SlideShow slideShowForm = new SlideShow(this.photos);
+            slideShowForm.ShowDialog();
+
+        }
+
+        private void Click_DateSort(object sender, ColumnClickEventArgs e)
+        {
+            if(e.Column == 3)
+            {
+                order = !order;
+                this.photos = Application.sortDate(this.photos, order);
+            }
+        }
+
+        private void Click_ShowPreview(object sender, EventArgs e)
+        {
+            if(ListView1.SelectedItems.Count == 1)
+            {
+                PictureBox1.ImageLocation = ListView1.SelectedItems[0].SubItems[0].ToString();
+            }
+        }
+
     }
 }
-
-//private IPhotoRepository photoRepository;
-//private IAlbumRepository albumRepository;
-//private IPhotoFileService photoFileService;
-//private PhotoFrameApplication application;
-//private IEnumerable<Photo> searchedPhotos; // リストビュー上のフォトのリスト
-
-///// <summary>
-///// コンストラクタ
-///// </summary>
-//public Form1()
-//{
-//    InitializeComponent();
-
-//    // 各テストごとにデータベースファイルを削除
-//    // (35-42をコメントアウトしても動きます)
-//    if (System.IO.File.Exists("_Photo.csv"))
-//    {
-//        System.IO.File.Delete("_Photo.csv");
-//    }
-//    if (System.IO.File.Exists("_Album.csv"))
-//    {
-//        System.IO.File.Delete("_Album.csv");
-//    }
-
-//    // メンバ変数初期化
-//    RepositoryFactory repositoryFactory = new RepositoryFactory(PhotoFrame.Persistence.Type.Csv);
-//    ServiceFactory serviceFactory = new ServiceFactory(); 
-//    photoRepository = repositoryFactory.PhotoRepository;
-//    albumRepository = repositoryFactory.AlbumRepository;
-//    photoFileService = serviceFactory.PhotoFileService;
-//    application = new PhotoFrameApplication(albumRepository, photoRepository, photoFileService);
-//    searchedPhotos = new List<Photo>().AsEnumerable();
-
-//    // 全アルバム名を取得し、アルバム変更リストをセット
-//    IEnumerable<Album> allAlbums = albumRepository.Find((IQueryable<Album> albums) => albums);
-
-//    if(allAlbums != null)
-//    {
-//        foreach (Album album in allAlbums)
-//        {
-//            comboBox_ChangeAlbum.Items.Add(album.Name);
-//        }
-//    }
-
-//}
-
-///// <summary>
-///// アルバム名でフォトを検索
-///// </summary>
-///// <param name="sender"></param>
-///// <param name="e"></param>
-//private void button_Search_Click(object sender, EventArgs e)
-//{
-//    if (radioButton_AlbumName.Checked)
-//    {
-//        this.searchedPhotos = application.SearchAlbum(textBox_Search.Text);
-//    }
-//    else if (radioButton_DirectoryName.Checked)
-//    {
-//        this.searchedPhotos = application.SearchDirectory(textBox_Search.Text);
-//    }
-
-//    renewPhotoListView();
-//}
-
-///// <summary>
-///// アルバム新規作成
-///// </summary>
-///// <param name="sender"></param>
-///// <param name="e"></param>
-//private void button_CreateAlbum_Click(object sender, EventArgs e)
-//{
-//    string albumName = textBox_CreateAlbum.Text;
-//    int result = application.CreateAlbum(albumName);
-
-//    switch (result)
-//    {
-//        case 0:
-//            comboBox_ChangeAlbum.Items.Add(albumName);
-//            break;
-//        case 1:
-//            MessageBox.Show("アルバム名が未入力です");
-//            break;
-//        case 2:
-//            MessageBox.Show("既存のアルバム名です");
-//            break;
-//        default:
-//            break;
-//    }
-
-//}
-
-///// <summary>
-///// お気に入り切り替え
-///// </summary>
-///// <param name="sender"></param>
-///// <param name="e"></param>
-//private void button_ToggleFavorite_Click(object sender, EventArgs e)
-//{
-//    for (int i = 0; i < listView_PhotoList.SelectedItems.Count; i++)
-//    {
-//        int index = listView_PhotoList.SelectedItems[i].Index;
-
-//        Photo photo = application.ToggleFavorite(searchedPhotos.ElementAt(index));
-
-//        renewPhotoListViewItem(index, photo);
-//    }
-
-//}
-
-///// <summary>
-///// 選択中のフォトの所属アルバムを変更
-///// </summary>
-///// <param name="sender"></param>
-///// <param name="e"></param>
-//private async void button_ChangeAlbum_Click(object sender, EventArgs e)
-//{
-//    string newAlbumName = comboBox_ChangeAlbum.Text;
-//    var listviewindex = new List<int>();
-//    for (int i = 0; i < listView_PhotoList.SelectedItems.Count; i++)
-//    {
-//        listviewindex.Add(listView_PhotoList.SelectedItems[i].Index);
-//    }
-//    foreach(int index in listviewindex)
-//    {
-//        Photo photo = await application.ChangeAlbumAsync(searchedPhotos.ElementAt(index), newAlbumName);
-//        renewPhotoListViewItem(index, photo);
-//    }
-//}
-
-///// <summary>
-///// リストビュー上のフォトのスライドショーを実行
-///// </summary>
-///// <param name="sender"></param>
-///// <param name="e"></param>
-//private void button_SlideShow_Click(object sender, EventArgs e)
-//{
-//    if(this.searchedPhotos.Count() > 0)
-//    {
-//        SlideShow slideShowForm = new SlideShow(this.searchedPhotos);
-//        slideShowForm.ShowDialog();
-//    }
-
-//}
-
-///// <summary>
-///// リストビュー1行分更新
-///// </summary>
-///// <param name="index"></param>
-///// <param name="photo"></param>
-//private void renewPhotoListViewItem(int index, Photo photo)
-//{
-//    string albumName, isFavorite;
-
-//    if (photo.Album != null)
-//    {
-//        albumName = photo.Album.Name;
-//    }
-//    else
-//    {
-//        albumName = "";
-//    }
-
-
-//    if (photo.IsFavorite)
-//    {
-//        isFavorite = "★";
-//    }
-//    else
-//    {
-//        isFavorite = "";
-//    }
-
-//    listView_PhotoList.Items[index].SubItems[0].Text = photo.File.FilePath;
-//    listView_PhotoList.Items[index].SubItems[1].Text = albumName;
-//    listView_PhotoList.Items[index].SubItems[2].Text = isFavorite;
-//}
-
-///// <summary>
-///// リストビューの更新
-///// </summary>
-///// <param name="photos"></param>
-//private void renewPhotoListView()
-//{
-//    listView_PhotoList.Items.Clear();
-
-//    if (this.searchedPhotos != null)
-//    {
-//        foreach (Photo photo in searchedPhotos)
-//        {
-//            string albumName, isFavorite;
-
-//            if(photo.Album != null)
-//            {
-//                albumName = photo.Album.Name;
-//            }
-//            else
-//            {
-//                albumName = "";
-//            }
-
-
-//            if (photo.IsFavorite)
-//            {
-//                isFavorite = "★";
-//            }
-//            else
-//            {
-//                isFavorite = "";
-//            }
-
-//            string[] item = { photo.File.FilePath, albumName, isFavorite };
-//            listView_PhotoList.Items.Add(new ListViewItem(item));
-
-//        }
-//    }
-//}
