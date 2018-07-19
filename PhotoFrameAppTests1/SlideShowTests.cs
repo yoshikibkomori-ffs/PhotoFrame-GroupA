63e4397fa3a86d83982d9b2af858db3e2840dc7e
yoshikibkomori-ffs <yoshiki.b.komori@fujifilm.com>
2018-07-19 11:03:30 +0900

    0719_小森_01

---
+++ PhotoFrameAppTests1/SlideShowTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoFrameApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoFrameApp.Tests
+{
+    [TestClass()]
+    public class SlideShowTests
+    {
+        [TestMethod()]
+        public void PreviousButtonInactivateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
